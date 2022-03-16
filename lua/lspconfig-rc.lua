7bc4338ac1bb8132eb7b1ccfbbb576a947551b66
Rotthin <rotthin_dev@protonmail.com>
2022-03-17 00:39:44 +0100

    Removed code action keymap

--- lua/lspconfig-rc.lua
+++ lua/lspconfig-rc.lua
@@ -13,7 +13,6 @@ local on_attach = function(client, bufnr)
 	buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 	buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 	buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-	buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
 end
 
 local servers = { 'pyright', 'rust_analyzer', 'tsserver', 'ccls' }
